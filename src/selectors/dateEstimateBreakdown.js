66a7606fe88a12e3ebb4bff847e0b7a2f7995709
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-02-05 12:37:06 -0500

    small tweak to text in DateEstimator tooltip

--- src/selectors/dateEstimateBreakdown.js
+++ src/selectors/dateEstimateBreakdown.js
@@ -23,7 +23,7 @@ const dateEstimateBreakdown = (status) => {
             jsx = <span>You can file an appeal within 15 business days of receiving an agency's final response.</span>
             break;
         case 'appealFiled':
-            jsx = <span>The Office of Open Records is required to provide a final determination (ie. appeal decision) 30 calendar days after receiving an appeal.</span>;
+            jsx = <span>Final determination (ie. appeal decision) are due 30 calendar days after an appeal is filed.</span>;
             break;
         case 'extendedFinalDetermDate':
             jsx = <span>This date is based on the final determination date you entered above.</span>;
